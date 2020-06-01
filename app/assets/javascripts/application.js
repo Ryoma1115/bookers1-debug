773511df93dae2347886ea2617e7c2223338b74d
github Ryoma1115 <t9831703@gmail.com>
2020-06-01 20:41:50 +0900

    [Finish]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require rails-ujs
+//= require bootstrap-sprockets
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,55 +1,79 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: [:show, :edit, :update, :destroy]
-
+  # before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :ensure_correct_user,only: [:edit, :update, :destroy]
   # GET /books
   # GET /books.json
   def index
-    @book = Book.new
     @books = Book.all
+
+    @book = Book.new
+    @user = User.find(current_user.id)
   end
 
   # GET /books/1
   # GET /books/1.json
   def show
+      #bookで定義していたら投稿欄のところと重複した
+    @book_detail = Book.find(params[:id])
+      # 子から親を参照している
+    @user = @book_detail.user
+    @book = Book.new
   end
 
   # GET /books/1/edit
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:notice] = "You have creatad book successfully."
+      redirect_to book_path(book)
+    else
+      @book = book
+      render :edit
+    end
   end
 
   # POST /books
   # POST /books.json
   def create
-    @book = Book.new(book_params)
-    if @book.save
-      redirect_to @book, notice: 'Book was successfully created.'
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    if book.save
+      flash[:notice] = "You have creatad book successfully."
+      redirect_to book_path(book)
     else
+      @book = book
+      @books = Book.all
+      @user = current_user
       render :index
-    end
-  end
-
-  # PATCH/PUT /books/1
-  # PATCH/PUT /books/1.json
-  def update
-    if @book.update()
-      redirect_to @book, notice: 'Book was successfully updated.'
-    else
-      render :edit
     end
   end
 
   # DELETE /books/1
   # DELETE /books/1.json
   def destroy
-    @book.destroy
-    redirect_to books_url, notice: 'Book was successfully destroyed.'
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+
+  def ensure_correct_user
+    book = Book.find(params[:id])
+    if book.user_id != current_user.id
+      redirect_to books_path
+    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_book
-      @book = Book.find(params[:id])
-    end
+    # # Use callbacks to share common setup or constraints between actions.
+    # def set_book
+    #   @book = Book.find(params[:id])
+    # end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
